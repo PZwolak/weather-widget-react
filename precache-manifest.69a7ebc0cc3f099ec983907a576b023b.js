c42e4c06fa3b8f09746521727105efc728a50619
Patryk Zwolak <p.zwolak.93@gmail.com>
2020-05-20 21:38:24 +0200

    Updates

---
+++ precache-manifest.69a7ebc0cc3f099ec983907a576b023b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c0fdb1dbb3bc9796bec39458e0e6956",
+    "url": "/weather-widget-react/index.html"
+  },
+  {
+    "revision": "8a2c07458ef23e70edef",
+    "url": "/weather-widget-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9f27dcc06ec447c0dc13",
+    "url": "/weather-widget-react/static/css/main.1df9fa28.chunk.css"
+  },
+  {
+    "revision": "8a2c07458ef23e70edef",
+    "url": "/weather-widget-react/static/js/2.d08c3f9e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/weather-widget-react/static/js/2.d08c3f9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f27dcc06ec447c0dc13",
+    "url": "/weather-widget-react/static/js/main.b44fade2.chunk.js"
+  },
+  {
+    "revision": "7f470cef22ab23006036",
+    "url": "/weather-widget-react/static/js/runtime-main.ea978205.js"
+  }
+]);
